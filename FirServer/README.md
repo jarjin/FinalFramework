1503cf85f29e5a264708575110826c1ee912e5af
lijun10 <lijun10@bilibili.com>
2023-03-14 15:15:08 +0800

    update

---
+++ FirServer/README.md
@@ -0,0 +1,7 @@
+## Getting Started
+
+（1）设置环境变量 JAVA_HOME
+
+（2）VSCode打开服务器工程。
+
+（3）设置引用的jar包路径，protobuf-java-3.5.1.jar    sfs2x-core.jar     sfs2x.jar
---
+++ Protobuf/build.bat
@@ -0,0 +1,4 @@
+protoc -I=.\ --java_out=.\ .\common.proto
+protoc -I=.\ --java_out=.\ .\pbuser.proto
+protoc -I=.\ --csharp_out=.\ .\common.proto
+protoc -I=.\ --csharp_out=.\ .\pbuser.proto
