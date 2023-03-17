42c0d20487850ec5f02440b175644f881f03be01
lijun10 <lijun10@bilibili.com>
2023-03-17 15:00:49 +0800

    UPDATE

--- FirServer/FirServer/src/com/gamelogic/handler/LoginHandler.java
+++ FirServer/FirServer/src/com/gamelogic/handler/LoginHandler.java
@@ -4,6 +4,9 @@ import com.google.protobuf.InvalidProtocolBufferException;
 import com.handler.BaseHandler;
 import com.protos.Person;
 import com.smartfoxserver.v2.entities.User;
+import com.smartfoxserver.v2.entities.data.ISFSObject;
+import com.smartfoxserver.v2.entities.data.SFSObject;
+import com.tables.enums.ProtoType;
 
 public class LoginHandler extends BaseHandler {
     @Override
@@ -17,6 +20,8 @@ public class LoginHandler extends BaseHandler {
         if (person != null) {
             logMgr().Trace("Person Count: " + person.getName());
         }
+        ISFSObject reply = new SFSObject();
+        netMgr().SendData(user, ProtoType.LuaProtoMsg, reply);
     }
 }
 
